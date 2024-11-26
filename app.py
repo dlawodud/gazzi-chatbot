e0200607c5eaf0718001e4622a8f76e8e7c313ca
dlawodud <ljy7223@khu.ac.kr>
2024-11-26 22:22:06 +0900

    Add app.py

--- app.py
+++ app.py
@@ -1,12 +1,44 @@
-# bash : streamlit run 01_streamlit_test.py --server.port 8501
+# bash : streamlit run app.py --server.port 8501
 
 import streamlit as st
+from langchain_community.chat_models import ChatOllama
 from langchain_core.messages import ChatMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain.prompts import ChatPromptTemplate
+
+class ChatLLM:
+    def __init__(self):
+        # Model
+        self._model = ChatOllama(model="gemma2:2b", temperature=3)
+
+        # Prompt
+        self._template = """주어진 질문에 짧고 간결하게 한글로 답변을 제공해주세요.
+
+            Question: {question}
+        """
+        self._prompt = ChatPromptTemplate.from_template(self._template)
+
+        # Chain 연결
+        self._chain = (
+            {"question": RunnablePassthrough()}
+            | self._prompt
+            | self._model
+            | StrOutputParser()
+        )
+
+    def invoke(self, user_input):
+        response = self._chain.invoke(user_input)
+        return response
+    
+    def format_docs(self, docs):
+        return "\n\n".join([d.page_content for d in docs])
 
 class ChatWeb:
-    def __init__(self, page_title="Gazzi Chatbot", page_icon=":books:"):
+    def __init__(self, llm, page_title="Gazzi Chatbot", page_icon=":books:"):
         self._page_title = page_title
         self._page_icon = page_icon
+        self._llm = llm
 
     def print_messages(self):
         if "messages" in st.session_state and len(st.session_state["messages"]) > 0:
@@ -30,13 +62,15 @@ class ChatWeb:
             # 사용자가 입력한 내용
             st.chat_message("user").write(f"{user_input}")
             st.session_state["messages"].append(ChatMessage(role="user", content=user_input))
+            response = self._llm.invoke(user_input)
 
             # AI의 답변
             with st.chat_message("assistant"):
-                msg_assistant = f"당신이 입력한 내용: {user_input}"
+                msg_assistant = response
                 st.write(msg_assistant)
                 st.session_state["messages"].append(ChatMessage(role="assistant", content=msg_assistant))
 
 if __name__ == "__main__":
-    web = ChatWeb()
+    llm = ChatLLM()
+    web = ChatWeb(llm=llm)
     web.run()
